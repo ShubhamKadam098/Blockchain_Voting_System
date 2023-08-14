646fbb85bf8e2c36b9b87ab4f4d9481e53cca53b
Shubham Kadam <kadamshubham098@gmail.com>
2023-08-14 21:35:07 +0530

    Added Time and Candidates List Module

--- Voting_System/src/App.jsx
+++ Voting_System/src/App.jsx
@@ -3,11 +3,16 @@ import { ethers } from "ethers";
 import { contractABI, contractAddress } from "./Constant/constants.js";
 import Login from "./Components/login.jsx";
 import Connected from "./Components/Connected.jsx";
+import { parseTransaction } from "ethers/lib/utils.js";
 
 function App() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
+  const [votingStatus, setVotingStatus] = useState(false);
+  const [RemainingTime, setRemainingTime] = useState("");
+  const [candidates, setCandidates] = useState([]);
+  const [number, setNumber] = useState("");
 
   // Connect to metamask wallet
   async function connectWallet() {
@@ -31,6 +36,10 @@ function App() {
 
   // Defining actions after account is changed
   useEffect(() => {
+    fetchCandidates();
+    fetchCurrentStatus();
+    fetchRemainingTime();
+
     if (window.ethereum) {
       window.ethereum.on("accountsChanged", handleAccountsChanged);
     }
@@ -55,10 +64,57 @@ function App() {
     }
   }
 
+  // Fetching Current Status of the Contract
+  async function fetchCurrentStatus() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+    const status = await contract.getVotingStatus();
+    setVotingStatus(status);
+  }
+  // Getting Remaining Time
+  async function fetchRemainingTime() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+    const time = await contract.getRemainingTime();
+    setRemainingTime(parseInt(time, 16));
+  }
+
+  // Fetching Candidate Information
+  async function fetchCandidates() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+    const candidatesList = contract.getAllVotesOfCandiates();
+    const formatedList = candidatesList.map((candidate, index) => {
+      return {
+        index: index,
+        name: candidate.name,
+        voteCount: candidate.voteCount.toNumber(),
+      };
+    });
+    setCandidates(formatedList);
+  }
+
+  // Handle input number change
+  async function handleNumberChange(e) {
+    setNumber(e.target.value);
+  }
+
   return (
     <>
       {isConnected ? (
-        <Connected accountNumber={account} />
+        <Connected
+          accountNumber={account}
+          candidates={candidates}
+          RemainingTime={RemainingTime}
+          number={number}
+          handleNumberChange={handleNumberChange}
+        />
       ) : (
         <Login connectWallet={connectWallet} />
       )}
--- Voting_System/src/Components/Connected.jsx
+++ Voting_System/src/Components/Connected.jsx
@@ -7,6 +7,34 @@ export default function Connected(props) {
       <div className="container">
         <h1>You Are Connected to Metamask</h1>
         <p>Account Number: {props.accountNumber}</p>
+        <p> Remaining Time: {props.RemainingTime}</p>
+        <input
+          type="number"
+          placeholder="Entern Candidate Index"
+          value={props.number}
+          onChange={props.handleNumberChange}
+        ></input>
+        <button className="login-button" onChange={props.voteFunction}>
+          Vote
+        </button>
+        <table id="myTable" className="candidates-table">
+          <thead>
+            <tr>
+              <th>Index</th>
+              <th>Candidate name</th>
+              <th>Candidate votes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.candidates.map((candidate, index) => (
+              <tr key={index}>
+                <td>{candidate.index}</td>
+                <td>{candidate.name}</td>
+                <td>{candidate.voteCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
