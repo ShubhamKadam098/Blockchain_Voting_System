23068786dd2734ea0071046c7e5f21b616a211ed
Shubham Kadam <kadamshubham098@gmail.com>
2023-08-17 20:50:28 +0530

    Added wrapper classes and css styling class

--- Voting_System/src/Components/Login.jsx
+++ Voting_System/src/Components/Login.jsx
@@ -1,17 +1,18 @@
-import { ethers } from "ethers";
-import React, { useState } from "react";
+import React from "react";
 
 export default function Login(props) {
   return (
-    <>
-      <h1>Welcome to Election 2024</h1>
-      <button
-        id="connectWalletBtn"
-        className="btn connectbtn"
-        onClick={props.connectWallet}
-      >
-        Login
-      </button>
-    </>
+    <div className="loginWrapper">
+      <div className="loginSection">
+        <h1>Welcome to Election 2024</h1>
+        <button
+          id="connectWalletBtn"
+          className="btn connectbtn"
+          onClick={props.connectWallet}
+        >
+          Login
+        </button>
+      </div>
+    </div>
   );
 }
