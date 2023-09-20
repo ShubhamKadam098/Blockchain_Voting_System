7dcef8316729d9574b39efb49382dad2766411d3
Shubham Kadam <kadamshubham098@gmail.com>
2023-09-20 17:43:22 +0530

    Added component in main page

--- Admin Panel/src/App.jsx
+++ Admin Panel/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import LoginSection from "./components/LoginSection";
 import "./styles/main.css";
+import Navbar from "./components/Navbar";
+import VoterListSection from "./components/VoterListSection";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <LoginSection></LoginSection>
+      <VoterListSection></VoterListSection>
     </>
   );
 }
