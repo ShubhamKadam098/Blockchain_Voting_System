78dae2625a6dafb6b03b3763237ba299656a05ec
Shubham Kadam <kadamshubham098@gmail.com>
2023-09-21 21:39:47 +0530

    Added funtionality to add the data of the user to the database

--- Admin Panel/src/components/AddVoter.jsx
+++ Admin Panel/src/components/AddVoter.jsx
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import { db, storage } from "../config/firebase.js";
 import { setDoc, doc, getDoc } from "firebase/firestore";
 import { ref, uploadBytes } from "firebase/storage";
+import "../styles/addVoter.css";
 
 const AddVoter = () => {
   const [voterName, setVoterName] = useState("");
@@ -13,13 +14,62 @@ const AddVoter = () => {
   const [voterFingerprint, setVoterFingerprint] = useState(null);
   const addVoterForm = useRef(null);
 
+  const addVoter = async (e) => {
+    e.preventDefault();
+    if (
+      voterName == null ||
+      voterAge == null ||
+      voterAadharNumber == null ||
+      voterPin == null ||
+      voterProfile == null
+    ) {
+      alert("All fields are required!");
+      return;
+    }
+
+    // Database References
+    const voterRef = doc(db, "Voters", voterAadharNumber);
+    const voterProfileRef = ref(storage, `Profile/${voterAadharNumber}`);
+    const docSnapshot = await getDoc(voterRef);
+
+    if (docSnapshot.exists()) {
+      // Voter already exists
+      alert("Voter already exists, cannot add a duplicate voter.");
+      console.log("Document already exists, cannot add a duplicate.");
+    } else {
+      // Voter doesn't exist. Uploading data to database
+      try {
+        await setDoc(voterRef, {
+          AadharNumber: voterAadharNumber,
+          Age: voterAge,
+          Name: voterName,
+          Pin: voterPin,
+        });
+      } catch (error) {
+        console.error(`Error while add data in database: ${error}`);
+        return;
+      }
+      // Uploading profile image to storage
+      try {
+        await uploadBytes(voterProfileRef, voterProfile);
+      } catch (error) {
+        console.log(
+          `Error while uploading profile image to database: ${error}`
+        );
+        return;
+      }
+      alert("Congratulation! Voter has been added successfully.");
+      resetForm();
+    }
+  };
+
   return (
     <section id="addVoterSection">
       <div className="container">
         <button className=" btn backBtn">
           <img src="./src/assets/back.png" /> Back
         </button>
-        <form id="addVoterForm" ref={addVoterForm}>
+        <form id="addVoterForm" ref={addVoterForm} onSubmit={addVoter}>
           <h3 className="addVoterHeading">Add User</h3>
           <div className="formInput">
             <label htmlFor="name">Name</label>
