7293d870e948402394cc5933c80bcbfad5eda418
Shubham Kadam <kadamshubham098@gmail.com>
2023-09-28 01:06:22 +0530

    Added auto login funtionality in login component

--- Admin Panel/src/App.jsx
+++ Admin Panel/src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { auth } from "./config/firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
 import LoginSection from "./components/LoginSection.jsx";
 import Main from "./components/MainPage.jsx";
 import Navbar from "./components/Navbar.jsx";
@@ -8,6 +10,20 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [selectedViewVoter, setSelectedViewVoter] = useState(null);
   const [selectedUpdateVoter, setSelectedUpdateVoter] = useState(null);
+
+  // Use Firebase Auth State Listener
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user.email);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+
+    // Unsubscribe to the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
