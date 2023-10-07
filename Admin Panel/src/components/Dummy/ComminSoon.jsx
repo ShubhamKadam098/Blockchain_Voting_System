a3da7b397ad55e00e3e692c0c567bc19eee512ea
Shubham Kadam <kadamshubham098@gmail.com>
2023-10-08 01:36:20 +0530

    Added Comming soon page

---
+++ Admin Panel/src/components/Dummy/ComminSoon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComminSoon = () => {
+  return (
+    <div className="h-screen w-auto m-auto flex items-center justify-center">
+      <h1 className="text-2xl">Comming Soon ⏰...</h1>
+    </div>
+  );
+};
+
+export default ComminSoon;
