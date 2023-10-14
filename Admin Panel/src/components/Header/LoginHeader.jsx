730294460aeb4f21ec641fdfb30d66ea73fa52d8
Shubham Kadam <kadamshubham098@gmail.com>
2023-10-14 19:03:30 +0530

    Added component for login page header

---
+++ Admin Panel/src/components/Header/LoginHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../Header/Logo";
+
+const LoginHeader = () => {
+  return (
+    <div className="bg-slate-900 text-white object-contain p-4">
+      <Logo></Logo>
+    </div>
+  );
+};
+
+export default LoginHeader;
