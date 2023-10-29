8ced7628051570c9777628d9cfdb51f7c056093d
Shubham Kadam <kadamshubham098@gmail.com>
2023-10-29 22:40:37 +0530

    Added delete voter funtionality

--- Admin Panel/src/components/Pages/VoterList/TableRow.jsx
+++ Admin Panel/src/components/Pages/VoterList/TableRow.jsx
@@ -7,6 +7,42 @@ const TableRow = ({
   dob = "01-01-2000",
   city = "N/A",
 }) => {
+  // Remove Voter
+  const removeVoter = async (id) => {
+    if (
+      !window.confirm(
+        `Are you sure you want to delete the voter?\nVoter ID: ${id}`
+      )
+    ) {
+      console.log("Operation Cancelled");
+      return;
+    }
+
+    try {
+      const voterDoc = doc(db, "Voters", id);
+      await deleteDoc(voterDoc);
+
+      const profileRef = ref(storage, `Profile/${id}`);
+      await deleteObject(profileRef);
+
+      const folderPath = `VoterBiometrics/${id}`;
+      const storageRef = ref(storage, folderPath);
+
+      // Try to delete the folder, even if it doesn't exist
+      await deleteObject(storageRef).catch((error) => {
+        if (error.code !== "storage/object-not-found") {
+          throw error;
+        }
+      });
+
+      // Recursively delete all objects inside the folder
+      await deleteObjectsInFolder(storageRef);
+
+      getVoters(); // Update the list of voters after deletion
+    } catch (error) {
+      console.error(`Error while deleting voter details: ${error}`);
+    }
+  };
   return (
     <tr className="bg-white hover:bg-gray-50" key={aadharNumber}>
       <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
@@ -32,7 +68,12 @@ const TableRow = ({
         >
           Update
         </Link>
-        <button className="font-normal text-xs align-middle self-center text-white bg-red-600 rounded-full py-2 px-4 hover:underline">
+        <button
+          onClick={() => {
+            removeVoter(aadharNumber);
+          }}
+          className="font-normal text-xs align-middle self-center text-white bg-red-600 rounded-full py-2 px-4 hover:underline"
+        >
           Delete
         </button>
       </td>
