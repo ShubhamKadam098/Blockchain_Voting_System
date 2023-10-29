b2c535a0bf878794e6eb546c5033607eb55ea5b0
Shubham Kadam <kadamshubham098@gmail.com>
2023-10-29 22:16:34 +0530

    Added redirect links for table row

--- Admin Panel/src/components/Pages/VoterList/TableRow.jsx
+++ Admin Panel/src/components/Pages/VoterList/TableRow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const TableRow = ({
   name = "Undefined",
@@ -19,12 +20,18 @@ const TableRow = ({
       <td className="px-6 py-4">{dob}</td>
 
       <td className="px-6 py-4  flex items-center gap-4 w-min">
-        <button className="font-normal text-xs align-middle self-center text-white bg-green-600 rounded-full py-2 px-4 hover:underline">
+        <Link
+          to={`/view_voter?voterID=${aadharNumber}`}
+          className="font-normal text-xs align-middle self-center text-white bg-green-600 rounded-full py-2 px-4 hover:underline"
+        >
           View
-        </button>
-        <button className="font-normal text-xs align-middle self-center text-white bg-orange-600 rounded-full py-2 px-4 hover:underline">
+        </Link>
+        <Link
+          to={`/update_voter?voterID=${aadharNumber}`}
+          className="font-normal text-xs align-middle self-center text-white bg-orange-600 rounded-full py-2 px-4 hover:underline"
+        >
           Update
-        </button>
+        </Link>
         <button className="font-normal text-xs align-middle self-center text-white bg-red-600 rounded-full py-2 px-4 hover:underline">
           Delete
         </button>
