65d759f1bf356d67759f44122b8a6f19067a479b
Shubham Kadam <kadamshubham098@gmail.com>
2023-11-27 17:07:18 +0530

    Created 404 error dummy component

--- Voting_System/src/App.jsx
+++ Voting_System/src/App.jsx
@@ -10,6 +10,7 @@ import Login from "./components/Login/Login";
 import { UserProvider } from "./Context/UserContext";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import PrivateRoute from "./Components/Login/PrivateRoute";
+import PageNotFound from "./Components/Dummy/PageNotFound";
 
 function App() {
   const router = createBrowserRouter(
@@ -17,6 +18,7 @@ function App() {
       <>
         <Route path="/" element={<PrivateRoute element={<Dashboard />} />} />
         <Route path="/login" element={<Login />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
       </>
     )
   );
