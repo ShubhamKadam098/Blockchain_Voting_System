b0fe63125e20edee95524f3893faa8f4e18b0ac5
Shubham Kadam <kadamshubham098@gmail.com>
2023-11-29 15:56:38 +0530

    Added file for keeping all party logo url

---
+++ Voting_System/src/Constant/partyUrl.js
@@ -0,0 +1,11 @@
+const partiesURL = {
+  BJP: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Bharatiya_Janata_Party_logo.svg/180px-Bharatiya_Janata_Party_logo.svg.png",
+  INC: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Indian_National_Congress_hand_logo.svg/150px-Indian_National_Congress_hand_logo.svg.png",
+  MNS: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Mns-symbol-railway-engine.png/150px-Mns-symbol-railway-engine.png",
+  ShivSena:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Logo_of_Shiv_Sena.svg/88px-Logo_of_Shiv_Sena.svg.png",
+  AAP: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Aam_Aadmi_Party_logo_%28English%29.svg/180px-Aam_Aadmi_Party_logo_%28English%29.svg.png",
+  NCP: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Flag_of_Nationalist_Congress_Party.svg/180px-Flag_of_Nationalist_Congress_Party.svg.png",
+};
+
+export { partiesURL };
